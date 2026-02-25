ba3e54050792de19a1a02fe4df56afc297cd5ff5
celitix <developer@proactivedigital.in>
2026-02-25 15:17:14 +0530

    canonical tags added

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,10 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Mehrangarh Fort | Forts In India | Rajasthan Fort | Forts Of Rajasthan",
   description: "Explore the majestic Mehrangarh Fort in Jodhpur — one of India’s largest and most stunning forts. Discover its rich history, grand architecture, breathtaking city views, and must-visit attractions for an unforgettable Rajasthan experience.",
-   icons: {
+    alternates: {
+    canonical: "https://mehrangarhfort.in/",
+  },
+  icons: {
     icon: [
       { url: '/mehran-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/mehran-192x192.png', sizes: '192x192', type: 'image/png' },
